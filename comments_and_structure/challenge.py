c4763e15300506a33e43d2ae9ab83bb74d0bb756
martingrzzler <martin.gressler@code.berlin>
2021-11-13 18:59:15 +0100

    solution for comments and code structure

--- comments_and_structure/challenge.py
+++ comments_and_structure/challenge.py
@@ -1,15 +1,7 @@
 # (c) Maximilian Schwarzmüller / Academind GmbH
 
-# *********
-# Imports
-# *********
 from os import path, makedirs
 from pathlib import Path
-
-# *********
-# Main
-# *********
-# A class which allows us to create DiskStorage instances
 
 
 class DiskStorage:
@@ -19,17 +11,15 @@ class DiskStorage:
     def get_directory_path(self):
         return Path(self.storage_directory)
 
-    # This must be called before a file is inserted
     def create_directory(self):
         if (not path.exists(self.get_directory_path())):
             makedirs(self.storage_directory)
 
-    # Warning: Directory must exist in advance
     def insert_file(self, file_name, content):
+        self.create_directory()
         file = open(self.get_directory_path() / file_name, 'w')
         file.write(content)
         file.close()
-        # Todo: Add proper error handling
 
 
 log_storage = DiskStorage('logs')
